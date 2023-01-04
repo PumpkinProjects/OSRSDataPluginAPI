0503874ff389a544c54649a5548e7a0ab30624d1
pancakepuncher <pancakepuncher@gmail.com>
2023-01-04 13:50:28 -0500

    Adjusted Recieve Route

--- backend/routes/data_routes/recieve.py
+++ backend/routes/data_routes/recieve.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 router = APIRouter(
     tags=["Database Routes"]
 )
 
 @router.post("/data/recieve")
-async def recieve(data):
+async def recieve(request: Request):
+
+    data = await request.json()
 
     print(data)
 
